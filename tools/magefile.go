d6ffd203f5ad90b64856de6e033d08b9b35818e1
Yves Brissaud <yves.brissaud@docker.com>
2022-11-07 17:35:29 +0100

    add tools using eunomie/dague
    Signed-off-by: Yves Brissaud <yves.brissaud@docker.com>

---
+++ tools/magefile.go
@@ -0,0 +1,82 @@
+//go:build mage
+
+package main
+
+import (
+	"context"
+
+	"github.com/magefile/mage/sh"
+
+	"github.com/eunomie/dague/types"
+	//mage:import
+	_ "github.com/eunomie/dague/target/lint"
+	//mage:import
+	"github.com/eunomie/dague/target/golang"
+
+	"github.com/magefile/mage/mg"
+)
+
+var (
+	SnykDesktopVersion = "1.1025.0"
+	SnykImageDigest    = "sha256:b979e1827473ce7675439213a918687ac532481c3370818bce61884735bdb09d"
+	commit             string
+	tagName            string
+	envVars            = map[string]string{
+		"CGO_ENABLED": "0",
+	}
+	buildFlags []string
+)
+
+func init() {
+	var err error
+	commit, err = sh.OutCmd("git", "rev-parse", "--short", "HEAD")()
+	if err != nil {
+		panic(err)
+	}
+
+	tagName, err = sh.Output("git", "describe", "--always", "--dirty", "--abbrev=10")
+	if err != nil {
+		panic(err)
+	}
+
+	buildFlags = []string{
+		"-trimpath",
+		"-ldflags=-s -w" +
+			" -X 'github.com/docker/scan-cli-plugin/internal.GitCommit=" + commit + "'" +
+			" -X 'github.com/docker/scan-cli-plugin/internal.Version=" + tagName + "'" +
+			" -X 'github.com/docker/scan-cli-plugin/internal/provider.ImageDigest=" + SnykDesktopVersion + "'" +
+			" -X 'github.com/docker/scan-cli-plugin/internal/provider.SnykDesktopVersion=" + SnykImageDigest + "'",
+	}
+
+}
+
+type Build mg.Namespace
+
+// Local builds local binary of docker-scan, for the running platform
+func (Build) Local(ctx context.Context) error {
+	return golang.Local(ctx, types.LocalBuildOpts{
+		Dir:        "dist",
+		In:         "./cmd/docker-scan",
+		Out:        "docker-scan",
+		EnvVars:    envVars,
+		BuildFlags: buildFlags,
+	})
+}
+
+// Cross builds docker-scan binaries for all the supported platforms
+func (Build) Cross(ctx context.Context) error {
+	return golang.Cross(ctx, types.CrossBuildOpts{
+		Dir:           "dist",
+		In:            "./cmd/docker-scan",
+		OutFileFormat: "docker-scan_%s_%s",
+		Platforms: []types.Platform{
+			{"linux", "amd64"},
+			{"linux", "arm64"},
+			{"darwin", "amd64"},
+			{"darwin", "arm64"},
+			{"windows", "amd64"},
+		},
+		EnvVars:    envVars,
+		BuildFlags: buildFlags,
+	})
+}
